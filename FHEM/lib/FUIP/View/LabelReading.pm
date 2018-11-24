c71205c748b623062f4ce54d44cc417a41d74bf4
Thorsten Pferdekaemper <thorsten@pferdekaemper.com>
2018-11-24 17:08:49 +0100

    LabelReading verbessert

--- FHEM/lib/FUIP/View/LabelReading.pm
+++ FHEM/lib/FUIP/View/LabelReading.pm
@@ -10,14 +10,16 @@ use parent -norequire, 'FUIP::View';
 sub getHTML($){
 	my ($self) = @_;
 	# show reading
-	my $result = "<table width='100%' class=\"fuip-color\" style='border:1px solid; border-radius:8px;'>";
+	my $result = '<table width="100%" class="fuip-color" style="border-spacing: 0px;';
+	$result .= 'border:1px solid; border-radius:8px;' if(not $self->{border} or $self->{border} eq "solid");
+	$result .= '">'."\n";
 	if($self->{icon}){
 		$result .= '<tr><td style="vertical-align:center">
 							<i class="fa '.$self->{icon}.' fuip-color" style="font-size:26px"></i>
-					</td><td><table>';
+					</td><td style="padding:0px;"><table style="border-collapse: collapse; border-spacing: 0px;">'."\n";
 	};
-	$result .= "<tr><td class=\"fuip-color\">".$self->{label}."</td></tr>
-				<tr><td><div data-type=\"label\" 
+	$result .= "<tr><td class=\"fuip-color\">".$self->{label}."</td></tr>" if($self->{label});
+	$result .= "<tr><td><div data-type=\"label\" 
 							 class=\"fuip-color\"
 							 data-device=\"".$self->{reading}{device}."\"
 							 data-get=\"".$self->{reading}{reading}."\">
@@ -32,7 +34,28 @@ sub getHTML($){
 	
 sub dimensions($;$$){
 	my $self = shift;
-	return (main::AttrVal($self->{fuip}{NAME},"baseWidth",142), 60);
+	# none: 17
+	# border: 19
+	# icon:	28
+	# border, icon: 30
+	# label: 34
+	# icon, label: 34
+	# border, label: 36
+	# border, icon, label: 36
+	
+	# border always +2
+	# label => 34
+	# else icon => 28
+	# else 17
+
+	my $height = 17;
+	if($self->{label}) {
+		$height = 34;
+	}elsif($self->{icon}) {
+		$height = 28;
+	};
+	$height += 2 if(not $self->{border} or $self->{border} eq "solid");	
+	return (main::AttrVal($self->{fuip}{NAME},"baseWidth",142), $height);
 };	
 	
 
@@ -48,6 +71,8 @@ sub getStructure($) {
 		{ id => "title", type => "text", default => { type => "field", value => "reading-reading"} },
 		{ id => "label", type => "text", default => { type => "field", value => "title"} },
 		{ id => "icon", type => "icon" },
+		{ id => "border", type => "text", options => [ "solid", "none" ], 
+			default => { type => "const", value => "solid" } }, 
 		{ id => "popup", type => "dialog", default=> { type => "const", value => "inactive"} }		
 		];
 };
