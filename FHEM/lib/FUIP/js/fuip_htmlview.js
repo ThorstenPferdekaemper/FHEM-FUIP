aa6d4ce0b92ed0c70541522521eb616d7258fe2c
Thorsten Pferdekaemper <thorsten@pferdekaemper.com>
2019-01-06 20:28:36 +0100

    HTML view auch für betagte Browser

--- FHEM/lib/FUIP/js/fuip_htmlview.js
+++ FHEM/lib/FUIP/js/fuip_htmlview.js
@@ -2,30 +2,30 @@
 function renderHtmlView(html,variables) {
 	// replace variables
 	// find matches for <fuip-field>...</fuip-field>
-	let fieldStrings = html.match(/<fuip-field.*?<\/fuip-field>/g);
+	var fieldStrings = html.match(/<fuip-field.*?<\/fuip-field>/g);
 	// something wrong?
 	// TODO: error message or so
 	if(!fieldStrings) fieldStrings = [];  // null if no match
-	for(let fieldString of fieldStrings){
+	for(var i = 0; i < fieldStrings.length; i++){
 		// TODO: The following checks partially mean that a field def was found,
 		//		but something is wrong. Maybe proper error message and do not 
 		//		change anything
-		let fieldDef = $(fieldString);
+		var fieldDef = $(fieldStrings[i]);
 		if(!fieldDef) continue; 
-		let id = fieldDef.attr("fuip-name");
+		var id = fieldDef.attr("fuip-name");
 		if(!id) continue;
-		let value;
+		var value;
 		if(variables.hasOwnProperty(id)) {
 			value = variables[id];
 		}else{
 			value = fieldDef.text();
 		};		
-		html = html.replace(fieldString,value);
+		html = html.replace(fieldStrings[i],value);
 	}; 
 	// create DOM node
 	// the following is supposed not to break the whole
 	// document in case something is wrong
-	let elem = document.createElement("div");
+	var elem = document.createElement("div");
 	elem.innerHTML = html;
 	document.write(elem.innerHTML);
 };
